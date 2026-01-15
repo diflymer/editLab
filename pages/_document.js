f1b19c5c904d02d7f8525a148a9eaac65246ec55
diflymer <dan-kull@mail.ru>
2026-01-15 14:30:27 +0300

    with new script

--- pages/_document.js
+++ pages/_document.js
@@ -13,30 +13,19 @@ export default function Document() {
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(m,e,t,r,i,k,a){
-                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106270317', 'ym');
 
-              ym(106270317, 'init', {
-                defer: false,  // ОТКЛЮЧАЕМ автоматическую отправку
-                webvisor: true,
-                clickmap: true,
-                trackLinks: true,
-                accurateTrackBounce: true,
-                ecommerce: "dataLayer",
-                ssr: true
-              });
+    ym(106270317, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
 
               // ⚠️ ДОБАВЛЯЕМ РУЧНОЙ HIT ДЛЯ ПРОВЕРКИ
-              // ym(106270317, 'hit', window.location.pathname, {
-              //   referer: document.referrer || '',
-              //   title: document.title
-              // });
+              // ym(106270317, 'hit');
 
-              console.log('Script success??');
+              console.log('with new script');
             `
           }}
         />
